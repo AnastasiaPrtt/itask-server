5bccca72659dffdf8532fb83afa59754fb4bf90d
Lina Alexandrova <ang.s.alexandrova@gmail.com>
2024-04-28 12:07:17 +0300

    feat: add new setting for cors

--- app.js
+++ app.js
@@ -4,11 +4,16 @@ const express = require('express')
 const logger = require('morgan')
 const path = require('path')
 const cookieParser = require('cookie-parser')
+const errorMiddleware = require('./src/v1/middlewares/error-middleware')
 
 const app = express()
 const secret = process.env.COOKIE_SECRET_KEY;
 
-app.use(cors())
+app.use(cors({
+	credentials: true,
+	origin: process.env.CLIENT_URL
+}))
+
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
@@ -16,5 +21,8 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.use(cookieParser(secret));
 
 app.use('/api', require('./src/v1/routes/index'))
+
+
+app.use(errorMiddleware)
 
 module.exports = app 
