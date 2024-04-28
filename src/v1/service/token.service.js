74b66a93bcb8d5ae5466b276ab0a09281ffbaeb3
Lina Alexandrova <ang.s.alexandrova@gmail.com>
2024-04-28 12:12:38 +0300

    feat: add route user

--- src/v1/service/token.service.js
+++ src/v1/service/token.service.js
@@ -1,23 +1,56 @@
 const jwt = require('jsonwebtoken');
-const db = require('../models/index');
+const db = require('../models');
 
-exports.generateTokens = async (payload) => {
-	const accessToken = jwt.sign(payload, process.env.JWT_ACCESS_TOKEN_SECRET_KEY, {
-		expiresIn: '30m'
-	})
-	const refreshToken = jwt.sign(payload, process.env.JWT_REFRESH_TOKEN_SECRET_KEY, {
-		expiresIn: '30d'
-	})
-	return {
-		accessToken, refreshToken
+class TokenService {
+	generateTokens(payload) {
+		const accessToken = jwt.sign(payload, process.env.JWT_ACCESS_TOKEN_SECRET_KEY, {
+			expiresIn: '30m'
+		})
+		const refreshToken = jwt.sign(payload, process.env.JWT_REFRESH_TOKEN_SECRET_KEY, {
+			expiresIn: '30d'
+		})
+		return {
+			accessToken, refreshToken
+		}
 	}
-};
 
-exports.saveToken = async (userId, refreshToken) => {
-	const tokenData = await db.Token.findOne({where: userId});
-	if(tokenData) {
-		tokenData.refreshToken = refreshToken;
-		return tokenData.save()
+	async validateRefreshToken(token) {
+		try {
+			const userData = jwt.verify(token, process.env.JWT_REFRESH_TOKEN_SECRET_KEY)
+			return userData
+		} catch (error) {
+			return null
+		}
 	}
-	const token = await db.Token.create({userId, refreshToken})
+
+	async validateAccessToken(token) {
+		try {
+			const userData = jwt.verify(token, process.env.JWT_ACCESS_TOKEN_SECRET_KEY)
+			return userData
+		} catch (error) {
+			return null
+		}
+	}
+
+	async saveToken(userId, refreshToken) {
+		const tokenData = await db.sequelize.models.tokens.findOne({ where: { userId } });
+		if (tokenData) {
+			tokenData.refreshToken = refreshToken;
+			return tokenData.save()
+		}
+		const token = await db.sequelize.models.tokens.create({ userId, refreshToken })
+		return token
+	}
+
+	async removeToken(refreshToken) {
+		const tokenData = await db.sequelize.models.tokens.destroy({ where: { refreshToken } })
+		return tokenData
+	}
+
+	async findToken(refreshToken) {
+		const tokenData = await db.sequelize.models.tokens.findOne({ where: { refreshToken } })
+		return tokenData
+	}
 }
+
+module.exports = new TokenService();
